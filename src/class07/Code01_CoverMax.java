e53e807ab3771af1f368c0ef125d389b18276473
lixiaoxuan <lixiaoxuan@oristand.com>
2021-06-07 17:13:27 +0800

    加强堆以及堆相关问题

---
+++ src/class07/Code01_CoverMax.java
@@ -0,0 +1,22 @@
+package class07;
+
+/**
+ * @author lixiaoxuan
+ * @description: 最大线段重合问题（用堆的实现）
+ * @date 2021/6/7 15:37
+ */
+public class Code01_CoverMax {
+
+//    给定很多线段，每个线段都有两个数[start, end]，
+//    表示线段开始位置和结束位置，左右都是闭区间
+//    规定：
+//      1）线段的开始和结束位置一定都是整数值
+//      2）线段重合区域的长度必须>=1
+//    返回线段最多重合区域中，包含了几条线段
+
+
+    public static int maxCover1(int[][] lines) {
+
+        return 0;
+    }
+}
---
+++ src/class07/HeapGreater.java
@@ -0,0 +1,9 @@
+package class07;
+
+/**
+ * @author lixiaoxuan
+ * @description: TODO
+ * @date 2021/6/7 17:12
+ */
+public class HeapGreater {
+}
