532447367eabc4adb20c952d2f51316d1cf90c4f
lixiaoxuan <lixiaoxuan@oristand.com>
2021-06-11 17:34:17 +0800

    手写前缀树以及非基于比较的计数，基数排序

--- src/class08/Code01_TrieTree.java
+++ src/class08/Code01_TrieTree.java
@@ -1,5 +1,7 @@
 package class08;
 
+import java.util.HashMap;
+
 /**
  * @author lixiaoxuan
  * @description: 手写前缀树
@@ -7,9 +9,286 @@ package class08;
  */
 public class Code01_TrieTree {
 
-    public static class Trie{
+    public static class Node1 {
         int pass = 0;
         int end = 0;
-        Trie[] nexts = new Trie[26];
+        Node1[] nexts = new Node1[26];
+    }
+
+    public static class Trie1 {
+
+        private Node1 root;
+
+        public Trie1() {
+            this.root = new Node1();
+        }
+
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            //root.pass+1
+            char[] chars = word.toCharArray();
+            Node1 curNode = root;
+            curNode.pass++;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (curNode.nexts[index] == null) {
+                    curNode.nexts[index] = new Node1();
+                }
+                curNode = curNode.nexts[index];
+                curNode.pass++;
+            }
+            curNode.end++;
+        }
+
+        public void delete(String word) {
+            //先判断是否存在
+            if (search(word) == 0) {
+                return;
+            }
+            char[] chars = word.toCharArray();
+            Node1 curNode = root;
+            curNode.pass--;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                curNode = curNode.nexts[index];
+                curNode.pass--;
+            }
+            curNode.end--;
+        }
+
+        //查看在前缀树中出现的次数
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chars = word.toCharArray();
+            Node1 curNode = root;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (curNode.nexts[index] == null) {
+                    return 0;
+                }
+                curNode = curNode.nexts[index];
+            }
+
+            return curNode.end;
+        }
+
+        //查看在前缀树中前缀出现的次数
+        public int prefixNumber(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chars = word.toCharArray();
+            Node1 curNode = root;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (curNode.nexts[index] == null) {
+                    return 0;
+                }
+                curNode = curNode.nexts[index];
+            }
+            return curNode.pass;
+        }
+    }
+
+    public static class Node2 {
+        int pass = 0;
+        int end = 0;
+        HashMap<Integer, Node2> nexts = new HashMap<>();
+    }
+
+    public static class Trie2 {
+        private Node2 root;
+
+        public Trie2() {
+            this.root = new Node2();
+        }
+
+        public void insert(String word) {
+            if (word == null) {
+                return;
+            }
+            //root.pass+1
+            char[] chars = word.toCharArray();
+            Node2 curNode = root;
+            curNode.pass++;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (!curNode.nexts.containsKey(index)) {
+                    curNode.nexts.put(index, new Node2());
+                }
+                curNode = curNode.nexts.get(index);
+                curNode.pass++;
+            }
+            curNode.end++;
+        }
+
+        public void delete(String word) {
+            if (search(word) == 0) {
+                return;
+            }
+            char[] chars = word.toCharArray();
+            Node2 curNode = root;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                curNode = curNode.nexts.get(index);
+                curNode.pass--;
+            }
+            curNode.end--;
+        }
+
+        public int search(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chars = word.toCharArray();
+            Node2 curNode = root;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (!curNode.nexts.containsKey(index)) {
+                    return 0;
+                }
+                curNode = curNode.nexts.get(index);
+            }
+            return curNode.end;
+        }
+
+        public int prefixNumber(String word) {
+            if (word == null) {
+                return 0;
+            }
+            char[] chars = word.toCharArray();
+            Node2 curNode = root;
+            int index = 0;
+            for (int i = 0; i < chars.length; i++) {
+                index = chars[i] - 'a';
+                if (!curNode.nexts.containsKey(index)) {
+                    return 0;
+                }
+                curNode = curNode.nexts.get(index);
+            }
+            return curNode.pass;
+        }
+
+    }
+
+
+    public static class Right {
+
+        private HashMap<String, Integer> box;
+
+        public Right() {
+            box = new HashMap<>();
+        }
+
+        public void insert(String word) {
+            if (!box.containsKey(word)) {
+                box.put(word, 1);
+            } else {
+                box.put(word, box.get(word) + 1);
+            }
+        }
+
+        public void delete(String word) {
+            if (box.containsKey(word)) {
+                if (box.get(word) == 1) {
+                    box.remove(word);
+                } else {
+                    box.put(word, box.get(word) - 1);
+                }
+            }
+        }
+
+        public int search(String word) {
+            if (!box.containsKey(word)) {
+                return 0;
+            } else {
+                return box.get(word);
+            }
+        }
+
+        public int prefixNumber(String pre) {
+            int count = 0;
+            for (String cur : box.keySet()) {
+                if (cur.startsWith(pre)) {
+                    count += box.get(cur);
+                }
+            }
+            return count;
+        }
+    }
+
+    // for test
+    public static String generateRandomString(int strLen) {
+        char[] ans = new char[(int) (Math.random() * strLen) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            int value = (int) (Math.random() * 6);
+            ans[i] = (char) (97 + value);
+        }
+        return String.valueOf(ans);
+    }
+
+    // for test
+    public static String[] generateRandomStringArray(int arrLen, int strLen) {
+        String[] ans = new String[(int) (Math.random() * arrLen) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = generateRandomString(strLen);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arrLen = 100;
+        int strLen = 20;
+        int testTimes = 100000;
+        for (int i = 0; i < testTimes; i++) {
+            String[] arr = generateRandomStringArray(arrLen, strLen);
+            Trie1 trie1 = new Trie1();
+            Trie2 trie2 = new Trie2();
+            Right right = new Right();
+            for (int j = 0; j < arr.length; j++) {
+                double decide = Math.random();
+                if (decide < 0.25) {
+                    trie1.insert(arr[j]);
+                    trie2.insert(arr[j]);
+                    right.insert(arr[j]);
+                } else if (decide < 0.5) {
+                    trie1.delete(arr[j]);
+                    trie2.delete(arr[j]);
+                    right.delete(arr[j]);
+                } else if (decide < 0.75) {
+                    int ans1 = trie1.search(arr[j]);
+                    int ans2 = trie2.search(arr[j]);
+                    int ans3 = right.search(arr[j]);
+                    if (ans1 != ans2 || ans2 != ans3) {
+                        System.out.println("Oops1!");
+                    }
+                } else {
+                    int ans1 = trie1.prefixNumber(arr[j]);
+                    int ans2 = trie2.prefixNumber(arr[j]);
+                    int ans3 = right.prefixNumber(arr[j]);
+                    if (ans1 != ans2 || ans2 != ans3) {
+                        System.out.println("Oops2!");
+                        System.out.println(arr[j]);
+                        System.out.println(ans1);
+                        System.out.println(ans2);
+                        System.out.println(ans3);
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("finish!");
+
     }
 }
--- src/class08/Code03_RadixSort.java
+++ src/class08/Code03_RadixSort.java
@@ -1,13 +1,161 @@
 package class08;
 
+import java.util.Arrays;
+
 /**
  * @author lixiaoxuan
  * @description: 基数排序(仅针对于正整数)
  * @date 2021/6/11 11:30
  */
 public class Code03_RadixSort {
+    //    时间复杂度	额外空间复杂度		稳定性
+//    选择排序		O(N^2)			O(1)		无
+//    冒泡排序		O(N^2)			O(1)		有
+//    插入排序		O(N^2)			O(1)		有
+//    归并排序		O(N*logN)		O(N)		有
+//    随机快排		O(N*logN)	  O(logN)		无
+//    堆排序		O(N*logN)		O(1)		无
+//========================================================
+//    计数排序		O(N)			O(M)		有
+//    基数排序		O(N)			O(N)		有
+
+//    1）不基于比较的排序，对样本数据有严格要求，不易改写
+//    2）基于比较的排序，只要规定好两个样本怎么比大小就可以直接复用
+//    3）基于比较的排序，时间复杂度的极限是O(N*logN)
+//    4）时间复杂度O(N*logN)、额外空间复杂度低于O(N)、且稳定的基于比较的排序是不存在的。
+//    5）为了绝对的速度选快排、为了省空间选堆排、为了稳定性选归并
+
 
     public static void radixSort(int[] arr) {
+        if(arr == null || arr.length<2){
+            return;
+        }
+        int maxBits = maxbits(arr);
+        int[] help = new int[arr.length];
+        for (int i = 1; i <=maxBits; i++) {
+            int[] count = new int[10];
+            for (int j = 0; j < arr.length; j++) {
+                int digit = getDigit(arr[j],i);
+                count[digit]++;
+            }
+            //计算count数组累加和
+            for (int j = 1; j < 10; j++) {
+                count[j] = count[j-1]+count[j];
+            }
+            // 根据count数组的数据将arr数据填写到help数组中
+            // arr数组从右往左遍历
+            for (int j = arr.length-1; j >=0 ; j--) {
+                int digit = getDigit(arr[j],i);
+                help[count[digit]-1] = arr[j];
+                count[digit]--;
+            }
+
+            for (int j=0;j<help.length;j++){
+                arr[j] = help[j];
+            }
+        }
+    }
+
+    public static int getDigit(int value, int radix) {
+        return (value / (int) (Math.pow(10, radix - 1)) % 10);
+    }
+
+
+    // 最大的位数
+    private static int maxbits(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int res = 0;
+        while (max != 0) {
+            max = max / 10;
+            res++;
+        }
+
+        return res;
+    }
+
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 10000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            int[] arr3 = copyArray(arr1);
+            radixSort(arr2);
+            comparator(arr3);
+            if (!isEqual(arr2, arr3)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                printArray(arr3);
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
 
     }
+
+
 }
